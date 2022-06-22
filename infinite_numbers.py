a37352c3f1c5d9c7b55acf63b3de8c292c668978
Ewelina Sitarczyk <aivellyn8@gmail.com>
2022-06-22 14:03:03 +0200

    Add solution for infinite_numbers

--- infinite_numbers.py
+++ infinite_numbers.py
@@ -1,5 +1,12 @@
-"""Write function that will generate continous fibonacci numbers. You are forbidden from using for and while"""
+"""Write function that will generate continous fibonacci numbers.
+   You are forbidden from using for and while."""
 
+# The generator will not be infinite, at some point it will hit recursion limit
+def fibonacci(n1=0, n2=1):
+    yield n1
+    yield from fibonacci(n1=n2, n2=n1+n2)
 
-def fibonacci():
-    pass
+# Demonstrate generating first 10 numbers
+gen = fibonacci()
+for i in range(10):
+    print(next(gen))
