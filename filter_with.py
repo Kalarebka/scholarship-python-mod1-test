b530d9dd02566161b17076493917f203eb4012f3
Ewelina Sitarczyk <aivellyn8@gmail.com>
2022-06-28 15:45:08 +0200

    Filter_with actually uses filter now

--- filter_with.py
+++ filter_with.py
@@ -2,7 +2,7 @@
    to objects (list and/or dicts) that contains keyword. Function must filter nested containers.
    Usage of recursion is required"""
 
-"""It's ugly but it works and does not use filter"""
+"""It's ugly but it works"""
 
 import json
 import pprint
@@ -29,20 +29,17 @@ def filter_with(data, keyword):
     # Base case
     if not has_nested_list_or_dict(data):
         if isinstance(data, list):
-            return [item for item in data if contains_keyword(item, keyword)]
+            return list(filter(lambda item: contains_keyword(item, keyword), data))
         elif isinstance(data, dict):
-            return {k: v for k, v in data.items() if contains_keyword(v, keyword)}
+            return dict(filter(lambda item: contains_keyword(item[1], keyword), data.items()))
     # Have to go deeper & data is a list?
     if isinstance(data, list):
         new_data = [filter_with(item, keyword) if is_list_or_dict(item) else item for item in data]
-        return [item for item in new_data if contains_keyword(item, keyword)]
+        return list(filter(lambda item: contains_keyword(item, keyword), new_data))
     # Have to go deeper & data is a dict?
     elif isinstance(data, dict):
         new_data = {key: filter_with(value, keyword) if is_list_or_dict(value) else value for key, value in data.items()}
-        return {k: v for k, v in new_data.items() if contains_keyword(v, keyword)}
-
-        # list(filter(lambda item: contains_keyword(item, keyword), new_data))
-        # dict(filter(lambda item: contains_keyword(item[1]), new_data.items()))
+        return dict(filter(lambda item: contains_keyword(item[1], keyword), new_data.items()))
 
 for item in filter_with(data, "Gloria"):
     pp.pprint(item)
